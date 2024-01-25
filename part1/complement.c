2ee76dbb236929a8cacf214a6899a230b31bccc1
arifoglu <arifoglu2020@gmail.com>
2024-01-25 14:13:45 +0000

    new things has been done

---
+++ part1/complement.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("Hello world\n");
+    printf("Hello %s \n","AA");
+    
+    
+    char name[31];
+    scanf("%s",name);
+    printf("hello %s \n",name);
+
+    char name2[31];
+    scanf("%s",name2);
+    printf("hello %s you look very good\n",name2);
+
+    // this is a line comment 
+    /*  this 
+        is a 
+        multiline 
+        comment 
+    */
+
+
+    return 0;
+}
+
---
+++ part1/new.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+        
+	printf("new file is all about VIM");
+
+	return 0;
+
+}	
+
