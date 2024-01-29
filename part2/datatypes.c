126eadaf5877658d964ba813189c0200e592d732
arifoglu <arifoglu2020@gmail.com>
2024-01-29 14:10:50 +0000

    datatypes finished

---
+++ part2/datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    //
+    printf("%i\n",1);       // 1
+    printf("%f\n",1.1111); // 1.111100
+    printf("%f\n",1.11111);// 1.111100
+    return 0;
+}
---
+++ part2/scientificn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    
+    //int people = 2.5e4 ; // 25000 
+    //printf("%i\n",people);
+
+    printf("How many people are there : ");
+    double people;
+
+    scanf("%lf", &people);
+    printf("%f\n",people); /// decimal notation
+    printf("%e\n",people); /// scientific notation
+    printf("%g\n",people); /// computer decides
+    
+
+    return 0;
+}
