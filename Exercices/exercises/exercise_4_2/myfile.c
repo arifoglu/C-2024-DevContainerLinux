544c2dbf44d3cc41911b540b0bcd46709d1523de
arifoglu <arifoglu2020@gmail.com>
2024-02-28 10:08:18 +0000

    4.2 exercices has been done

---
+++ Exercices/exercises/exercise_4_2/myfile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    int x;
+    printf("Enter a integer : ");
+    scanf("%d",&x);
+    (x%2 == 0)? printf("this is even => return 0 \n") : printf("this is odd =>return 1 \n");
+
+    return 0;
+}
