07f71b76ccda7a47c81fa0e1105223ef88cb19c0
arifoglu <arifoglu2020@gmail.com>
2024-03-07 09:35:57 +0000

    9.3 has been done

--- Exercices/exercises/exercise_9_3/myfile.c
+++ Exercices/exercises/exercise_9_3/myfile.c
@@ -3,21 +3,41 @@
 int main(){
     FILE *file = fopen("/workspaces/C-2024-DevContainerLinux/Exercices/exercises/exercise_9_3/mata.txt", "w");
 
-    if(file != NULL){
+    FILE *file2 = fopen("/workspaces/C-2024-DevContainerLinux/Exercices/exercises/exercise_9_3/matb.txt", "w");
+    
+    FILE *sum = fopen("/workspaces/C-2024-DevContainerLinux/Exercices/exercises/exercise_9_3/sum.usr", "w");
+
+    if(file != NULL)
+    {
         for(int i = 1 ; i <= 100;i++){
-             fprintf(file, "%d ",i);
+             fprintf(file , "%d ",i);
            (i%10 == 0) ? fprintf(file,"\n") : 0;
         };
     }
-    
-    FILE *file2 = fopen("/workspaces/C-2024-DevContainerLinux/Exercices/exercises/exercise_9_3/matb.txt", "w");
-
-    if(file != NULL){
-        for(int k = 1 ; k <= 100;k++){
-             fprintf(file, "%d ",k);
-           (k%10 == 0) ? fprintf(file,"\n") : 0;
+    if(file2 != NULL)
+    {
+        for(int i = 1 ; i <= 100;i++){
+             fprintf(file2, "%d ",i);
+           (i%10 == 0) ? fprintf(file2,"\n") : 0;
         };
     }
+    
+    int i,j,a,b;
+    if(file != NULL && file2 != NULL)
+    {
+        for(i = 0 ; i < 10 ; i++){
+            for(j = 0 ; j < 10 ; j++){
+                fscanf(file,"%d",&a);
+                fscanf(file2,"%d",&b);
+                (j == 9) ? fprintf(sum,"%d\n", a+b ) : fprintf(sum,"%d\t", a+b ) ; 
+            }
+        }
 
+    fclose(file);
+    fclose(file2);
+    fclose(sum);    
+    }
+
+    
     return 0;
 }
